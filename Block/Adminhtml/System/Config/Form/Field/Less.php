e3e091a0bc3624ef684b3aa3249f34174a992cdf
Vitaliy Dmytruk <vitaliy@mage2cloud.com>
2019-11-12 17:16:59 +0200

    New config fields - Htmlmixed and Javascript.

--- Block/Adminhtml/System/Config/Form/Field/Less.php
+++ Block/Adminhtml/System/Config/Form/Field/Less.php
@@ -2,13 +2,13 @@
 
 namespace Swissup\Codemirror\Block\Adminhtml\System\Config\Form\Field;
 
-class Htmlmixed extends Css
+class Less extends Css
 {
     /**
      * {@inheritdoc}
      */
     public function getMode()
     {
-        return 'htmlmixed';
+        return 'javascript';
     }
 }
