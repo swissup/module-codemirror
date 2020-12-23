c946fbd5c88e832837cafe7a540d989ec00ea853
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2020-12-23 11:14:34 +0200

    Contextmenu plugin added (Pagebuilder)

---
+++ view/base/web/js/form/element/editor-plugins/contextmenu.js
@@ -0,0 +1,66 @@
+define([
+    'jquery',
+    'uiRegistry'
+], function ($, registry) {
+    'use strict';
+
+    return function (component) {
+        var menu = $('<div class="CodeMirror-menu"></div>'),
+            cm = component.editor;
+
+        $(document.body).append(menu);
+
+        /**
+         * @param {Object} itemConfig
+         */
+        function addItem(itemConfig) {
+            var item = $('<label>' + itemConfig.label + '</label>');
+
+            if (itemConfig.icon) {
+                item.prepend('<span class="cm-icon">' + itemConfig.icon + '</span>');
+            }
+
+            if (itemConfig.class) {
+                item.addClass(itemConfig.class);
+            }
+
+            if (itemConfig.title) {
+                item.attr('title', itemConfig.title);
+            }
+
+            if (itemConfig.target) {
+                registry.get(component.parentName + '.' + itemConfig.target, function (element) {
+                    item.attr('for', element.uid);
+                });
+            } else if (itemConfig.handler) {
+                item.click(itemConfig.handler);
+            }
+
+            menu.append(item.get(0));
+
+            return item;
+        }
+
+        $.each(component.editorConfig.contextmenu || [], function () {
+            addItem(this);
+        });
+
+        cm.on('contextmenu', function (instance, event) {
+            event.preventDefault();
+
+            menu.addClass('shown')
+                .css({
+                    left: event.pageX + 2,
+                    top: event.pageY - 16
+                });
+        });
+
+        $(document.body).click(function () {
+            menu.removeClass('shown');
+        });
+
+        return {
+            addItem: addItem
+        };
+    };
+});
--- view/base/web/js/form/element/editor-plugins/directives.js
+++ view/base/web/js/form/element/editor-plugins/directives.js
@@ -7,8 +7,7 @@ define([
 ], function ($, _, ko, wrapper, $t) {
     'use strict';
 
-    var iconDisabled = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M9.504 1.132a1 1 0 01.992 0l1.75 1a1 1 0 11-.992 1.736L10 3.152l-1.254.716a1 1 0 11-.992-1.736l1.75-1zM5.618 4.504a1 1 0 01-.372 1.364L5.016 6l.23.132a1 1 0 11-.992 1.736L4 7.723V8a1 1 0 01-2 0V6a.996.996 0 01.52-.878l1.734-.99a1 1 0 011.364.372zm8.764 0a1 1 0 011.364-.372l1.733.99A1.002 1.002 0 0118 6v2a1 1 0 11-2 0v-.277l-.254.145a1 1 0 11-.992-1.736l.23-.132-.23-.132a1 1 0 01-.372-1.364zm-7 4a1 1 0 011.364-.372L10 8.848l1.254-.716a1 1 0 11.992 1.736L11 10.58V12a1 1 0 11-2 0v-1.42l-1.246-.712a1 1 0 01-.372-1.364zM3 11a1 1 0 011 1v1.42l1.246.712a1 1 0 11-.992 1.736l-1.75-1A1 1 0 012 14v-2a1 1 0 011-1zm14 0a1 1 0 011 1v2a1 1 0 01-.504.868l-1.75 1a1 1 0 11-.992-1.736L16 13.42V12a1 1 0 011-1zm-9.618 5.504a1 1 0 011.364-.372l.254.145V16a1 1 0 112 0v.277l.254-.145a1 1 0 11.992 1.736l-1.735.992a.995.995 0 01-1.022 0l-1.735-.992a1 1 0 01-.372-1.364z" clip-rule="evenodd" /></svg>',
-        iconEnabled = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" /></svg>',
+    var iconCheck = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" /></svg>',
         builtInDirectives = {};
 
     builtInDirectives.widget = {
@@ -80,8 +79,10 @@ define([
         });
 
         component.plugin('contextmenu').then(function (contextmenu) {
-            contextmenu.addItem({
-                label: 'Parse directives',
+            var item = contextmenu.addItem({
+                label: $t('Parse directives'),
+                icon: iconCheck,
+                class: status() ? 'active' : '',
 
                 /**
                  * Toggle directives parsing
@@ -90,11 +91,14 @@ define([
                     status(!status());
                 }
             });
+
+            status.subscribe(function (flag) {
+                item.toggleClass('active', flag);
+            });
         });
 
         component.plugin('toolbar').then(function (toolbar) {
-            var //buttonToggle,
-                useWidget = _.find(component.editorConfig.directives, function (directive) {
+            var useWidget = _.find(component.editorConfig.directives, function (directive) {
                     return directive.type === 'widget';
                 });
 
@@ -110,25 +114,6 @@ define([
                     }
                 });
             }
-
-            // buttonToggle = toolbar.addButton({
-            //     class: 'icon',
-
-            //     /**
-            //      * Toggle directives parsing
-            //      */
-            //     handler: function () {
-            //         status(!status());
-            //     }
-            // });
-
-            // status.subscribe(function (flag) {
-            //     if (flag) {
-            //         buttonToggle.attr('title', $t('Mode: Parsed HTML')).html(iconEnabled);
-            //     } else {
-            //         buttonToggle.attr('title', $t('Mode: Raw HTML')).html(iconDisabled);
-            //     }
-            // });
         });
 
         /**
